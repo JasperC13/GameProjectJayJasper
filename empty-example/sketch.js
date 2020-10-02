d160441b8b66e65ff4d1498b5710346cf46a5321
JasperC13 <71346473+JasperC13@users.noreply.github.com>
2020-10-02 09:42:49 -0700

    Added Class

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,4 +1,6 @@
 let soccerField;
+let p1;
+
 
 function preload() {
   soccerField = loadImage('assets/SoccerField.jpg');
@@ -9,4 +11,43 @@ function preload() {
 function setup() {
   createCanvas(720, 400);
   image(soccerField, 0, 0);
+
+  p1 = new Player1(200,200,3,3);
+}
+
+function draw(){
+  p1.drawMe();
+  p1.moveMe();
+}
+
+class Player1 {
+  constructor(x,y,xspeed,yspeed){
+    this.x = x;
+    this.y = y;
+    this.xspeed = xspeed;
+    this.yspeed = yspeed;
+  }
+
+  drawMe(){
+    fill("red");
+    ellipse(this.x,this.y,20,20);
+  }
+
+  moveMe(){
+    if (keyIsDown(UP_ARROW)){
+    this.y -= this.yspeed;
+ }
+
+ if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by yspeed
+     this.y += this.yspeed;
+ }
+
+ if (keyIsDown(RIGHT_ARROW)) { //if you hold the right arrow, move right by xspeed
+   this.x += this.xspeed;
+ }
+
+ if (keyIsDown(LEFT_ARROW)) { //if you hold the left arrow, move left by xspeed
+   this.x -= this.xspeed;
+ }
+  }
 }
