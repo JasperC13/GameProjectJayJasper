8d9b3a3ed03882225a1c3cffefc04ae07825e705
JayBrMason <68661207+JayBrMason@users.noreply.github.com>
2020-10-02 09:37:36 -0700

    Added Background

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,46 +1,12 @@
-let P1;
+let soccerField;
 
-function setup(){
-  createCanvas(500,400);
+function preload() {
+  soccerField = loadImage('assets/SoccerField.jpg');
 
-  P1 = new Player1(200,200,3,3);
 }
 
-function draw(){
-  background("green");
 
-  P1.drawMe();
-  P1.moveme();
-}
-
-class Player1 {
-  constructor(x,y,xspeed,yspeed){
-    this.x = x;
-    this.y = y;
-    this.xspeed = xspeed;
-    this.yspeed = yspeed;
-  }
-
-  drawMe(){
-    fill("red");
-    ellipse(this.x,this.y,20,20);
-  }
-
-  moveMe(){
-    if (keyIsDown(UP_ARROW)){
-    this.y -= this.yspeed;
- }
-
- if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by yspeed
-     this.y += this.yspeed;
- }
-
- if (keyIsDown(RIGHT_ARROW)) { //if you hold the right arrow, move right by xspeed
-   this.x += this.xspeed;
- }
-
- if (keyIsDown(LEFT_ARROW)) { //if you hold the left arrow, move left by xspeed
-   this.x -= this.xspeed;
- }
-  }
+function setup() {
+  createCanvas(720, 400);
+  image(soccerField, 0, 0);
 }
