da34850a0a1657911e3491fb308166666fcf65ae
JasperC13 <71346473+JasperC13@users.noreply.github.com>
2020-10-03 19:47:48 -0700

    Added second player class

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,23 +1,26 @@
 let soccerField;
 let p1;
+let p2;
 
 function preload() {
   soccerField = loadImage('assets/SoccerField.jpg');
-
 }
 
 
 function setup() {
   createCanvas(551, 368);
 
-
-  p1 = new Player1(95,184,3,3);
+  p1 = new Player1(90,184,3,3);
+  p2 = new Player2(461,184,3,3);
 }
 
 function draw(){
   image(soccerField, 0, 0)
   p1.drawMe();
   p1.moveMe();
+  p2.drawMe();
+  p2.moveMe();
+
 }
 
 class Player1 {
@@ -30,6 +33,38 @@ class Player1 {
 
   drawMe(){
     fill("red");
+    ellipse(this.x,this.y,20,20);
+  }
+
+  moveMe(){
+    if (keyIsDown(87)){
+    this.y -= this.yspeed;
+ }
+
+ if (keyIsDown(83)) { // if you hold the down arrow, move down by yspeed
+     this.y += this.yspeed;
+ }
+
+ if (keyIsDown(68)) { //if you hold the right arrow, move right by xspeed
+   this.x += this.xspeed;
+ }
+
+ if (keyIsDown(65)) { //if you hold the left arrow, move left by xspeed
+   this.x -= this.xspeed;
+ }
+  }
+}
+
+class Player2 {
+  constructor(x,y,xspeed,yspeed){
+    this.x = x;
+    this.y = y;
+    this.xspeed = xspeed;
+    this.yspeed = yspeed;
+  }
+
+  drawMe(){
+    fill("blue");
     ellipse(this.x,this.y,20,20);
   }
 
