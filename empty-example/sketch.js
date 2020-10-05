b0d2bf5bc7994fb5a7dd74b04a3799095eb988a4
JasperC13 <71346473+JasperC13@users.noreply.github.com>
2020-10-05 09:55:43 -0700

    Created ball class and worked on bouncing
    Needs more work

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,6 +1,7 @@
 let soccerField;
 let p1;
 let p2;
+let ball;
 
 function preload() {
   soccerField = loadImage('assets/SoccerField.jpg');
@@ -12,6 +13,7 @@ function setup() {
 
   p1 = new Player1(90,184,3,3);
   p2 = new Player2(461,184,3,3);
+  ball = new Ball(275.5,184,1,0);
 }
 
 function draw(){
@@ -20,6 +22,10 @@ function draw(){
   p1.moveMe();
   p2.drawMe();
   p2.moveMe();
+
+  ball.drawBall();
+  ball.moveBall();
+  ball.bouceBall();
 
 }
 
@@ -84,5 +90,37 @@ class Player2 {
  if (keyIsDown(LEFT_ARROW)) { //if you hold the left arrow, move left by xspeed
    this.x -= this.xspeed;
  }
+  }
+}
+
+class Ball {
+  constructor(x,y,xspeed,yspeed){
+    this.x = x;
+    this.y = y;
+    this.xspeed = xspeed;
+    this.yspeed = yspeed;
+  }
+
+  drawBall(){
+    fill("white");
+    ellipse(this.x,this.y,10,10);
+  }
+
+  moveBall(){
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  }
+
+  bouceBall(){
+    if(this.x >= p1.x-20 && this.x <= p1.x+20 && this.y > p1.y-20 && this.y < p1.y+20){
+      this.xspeed = -this.xspeed;
+      this.yspeed = -this.yspeed;
+        if(this.xspeed==-1){
+          this.x=this.x-5;
+        }
+        if(this.xspeed==1){
+          this.x=this.x+5
+        }
+    }
   }
 }
